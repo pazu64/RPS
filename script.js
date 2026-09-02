b5debd87d6ce0da2d27e18b87907632d4b563ffc
ilham <243327593+pazu64@users.noreply.github.com>
2026-09-02 15:32:17 +0700

    complete the rock paper scissors game

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 function getComputerChoice () {
-    let rps = ["rock", "papper", "scissors"];
+    let rps = ["rock", "paper", "scissors"];
     let randomInt = Math.floor(Math.random() * 3);
-    return rps[randomInt]
+    return rps[randomInt];
 }
 
 function getHumanChoice () {
     let humanChoice = prompt("enter your choice of rock, paper, or scissors");
-    return humanChoice
+    return humanChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+    let fixHumanChoice = humanChoice.toLowerCase();
+    if (fixHumanChoice ===  computerChoice) {
+        console.log(`A draw, ${fixHumanChoice} againts ${computerChoice}`);
+    }else if ((fixHumanChoice === "rock" && computerChoice === "scissors") || (fixHumanChoice === "paper" && computerChoice === "rock") || (fixHumanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore += 1;
+        console.log(`You win! ${fixHumanChoice} beats ${computerChoice}.`);
+    }else{
+        computerScore += 1;
+        console.log(`You lose! ${computerChoice} beats ${fixHumanChoice}`);
+    }
+}
+
+function playGame () {
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`Result : Human : ${humanScore}, Computer : ${computerScore}`)
+}
+playGame()
